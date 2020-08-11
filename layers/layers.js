bd15ae594e6a348e8cfdf0d20bbbda0e3eb5bad2
wansooim <61211242+wansooim@users.noreply.github.com>
2020-08-11 00:31:56 -0500

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,30 @@
+var wms_layers = [];
+
+var format_cb_2018_us_state_500k_0 = new ol.format.GeoJSON();
+var features_cb_2018_us_state_500k_0 = format_cb_2018_us_state_500k_0.readFeatures(json_cb_2018_us_state_500k_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_cb_2018_us_state_500k_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_cb_2018_us_state_500k_0.addFeatures(features_cb_2018_us_state_500k_0);
+var lyr_cb_2018_us_state_500k_0 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_cb_2018_us_state_500k_0, 
+                style: style_cb_2018_us_state_500k_0,
+                interactive: true,
+    title: 'cb_2018_us_state_500k<br />\
+    <img src="styles/legend/cb_2018_us_state_500k_0_0.png" /> 158340391 - 20205125364 <br />\
+    <img src="styles/legend/cb_2018_us_state_500k_0_1.png" /> 20205125364 - 106802728188 <br />\
+    <img src="styles/legend/cb_2018_us_state_500k_0_2.png" /> 106802728188 - 144661267977 <br />\
+    <img src="styles/legend/cb_2018_us_state_500k_0_3.png" /> 144661267977 - 212886221680 <br />\
+    <img src="styles/legend/cb_2018_us_state_500k_0_4.png" /> 212886221680 - 1478839695958 <br />'
+        });
+
+lyr_cb_2018_us_state_500k_0.setVisible(true);
+var layersList = [lyr_cb_2018_us_state_500k_0];
+lyr_cb_2018_us_state_500k_0.set('fieldAliases', {'STATEFP': 'STATEFP', 'STATENS': 'STATENS', 'AFFGEOID': 'AFFGEOID', 'GEOID': 'GEOID', 'STUSPS': 'STUSPS', 'NAME': 'NAME', 'LSAD': 'LSAD', 'ALAND': 'ALAND', 'AWATER': 'AWATER', 'GEOIDN': 'GEOIDN', });
+lyr_cb_2018_us_state_500k_0.set('fieldImages', {'STATEFP': 'TextEdit', 'STATENS': 'TextEdit', 'AFFGEOID': 'TextEdit', 'GEOID': 'TextEdit', 'STUSPS': 'TextEdit', 'NAME': 'TextEdit', 'LSAD': 'TextEdit', 'ALAND': 'TextEdit', 'AWATER': 'TextEdit', 'GEOIDN': 'Range', });
+lyr_cb_2018_us_state_500k_0.set('fieldLabels', {'STATEFP': 'no label', 'STATENS': 'no label', 'AFFGEOID': 'no label', 'GEOID': 'no label', 'STUSPS': 'no label', 'NAME': 'inline label', 'LSAD': 'no label', 'ALAND': 'no label', 'AWATER': 'no label', 'GEOIDN': 'no label', });
+lyr_cb_2018_us_state_500k_0.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/cb_2018_us_state_500k_0_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_cb_2018_us_state_500k_0 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("ALAND");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 158340391.000000 && value <= 20205125364.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 20205125364.000000 && value <= 106802728188.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,191,191,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 106802728188.000000 && value <= 144661267977.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,128,128,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 144661267977.000000 && value <= 212886221680.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,64,64,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 212886221680.000000 && value <= 1478839695958.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
